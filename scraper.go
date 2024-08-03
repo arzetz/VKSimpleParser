22eeff3ca9949b1251a93f8307e593502ec868d7
freeny-afk <artemkach@inbox.ru>
2024-08-03 12:44:43 +0300

    new verc

--- scraper.go
+++ scraper.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,16 +14,23 @@ import (
 )
 
 type User struct {
-	username, name, surname, birthdate, quote string
+	Username  string `json:"username"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Birthdate string `json:"birthdate,omitempty"`
+	Status    string `json:"status,omitempty"`
 }
 
 func main() {
-	var users []User
+	var link string
+	var filename string
 	service, err := selenium.NewChromeDriverService("./chromedriver", 4444)
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
-
+	flag.StringVar(&link, "u", "", "Enter the URL please")
+	flag.StringVar(&filename, "f", "", "Enter the filename, where you want info to be in in .json")
+	flag.Parse()
 	// configure the browser options
 	caps := selenium.Capabilities{}
 	caps.AddChrome(chrome.Capabilities{Args: []string{
@@ -35,38 +44,41 @@ func main() {
 	driver, _ := selenium.NewRemote(caps, "")
 	// maximize the current window to avoid responsive rendering
 	driver.MaximizeWindow("")
-	driver.Get("https://vk.com/shcherbakovv27")
+	driver.Get(link)
 	page, _ := driver.PageSource()
 
-	file, _ := os.Create("helloween.txt")
-
-	file.WriteString(page)
 	re := regexp.MustCompile(`"domain":"([^"]*)`)
 	user_username := re.FindStringSubmatch(page)
 	re = regexp.MustCompile(`title>([^|]*)`)
 	user_info := re.FindStringSubmatch(page) // Learning user name and surname
 	re = regexp.MustCompile(`"bdate":"([^"]*)`)
 	user_age := re.FindStringSubmatch(page)
-	fmt.Println(user_info)
+	re = regexp.MustCompile(`"activity":"(.*?)"`)
+	user_status := re.FindStringSubmatch(page)
 	if len(user_username) > 1 {
-		user := User{username: user_username[1]}
-		users = append(users, user)
-	}
-	if len(user_info) > 1 {
-		parts := strings.Split(user_info[1], " ")
-		users[0].name = parts[0]
-		users[0].surname = parts[1]
 
+		parts := strings.Split(user_info[1], " ")
+		user := User{
+			Username: user_username[1], // That's weird FindStringSubmatch structure, that's why we take like [1]
+			Name:     parts[0],
+			Surname:  parts[1],
+		}
+
+		if len(user_age) > 1 {
+			user.Birthdate = user_age[1]
+		}
+		if len(user_status) > 1 {
+			user.Status = user_status[1]
+		}
+		jsonData, _ := json.Marshal(user)
+		os.WriteFile(filename, jsonData, 0666)
+		fmt.Printf("\nEverything is cool! Check %s", filename)
 	}
-	if len(user_age) > 1 {
-		users[0].birthdate = user_age[1] // That's weird FindStringSubmatch structure, that's why we take like [1]
-	}
-	fmt.Println(users, "Done.")
+
 	defer func() {
-		fmt.Println("Bb") //Sorry but i don't give a foc
+		fmt.Println("\nHave a good BROOT") //Sorry but i don't give a foc
 		os.Exit(0)
 	}()
 	defer service.Stop()
 	defer driver.Quit()
-	defer file.Close()
 }
